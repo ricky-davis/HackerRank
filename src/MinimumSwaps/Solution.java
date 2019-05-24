95c8d1f6402ddc275e405e1a81bf37838d1481b6
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-24 10:34:27 -0500

    MinimumSwaps

---
+++ src/MinimumSwaps/Solution.java
@@ -0,0 +1,53 @@
+package MinimumSwaps;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int first = 0, last = arr.length - 1;
+        int swaps = 0;
+        while (first < last) {
+            while (arr[first] == first + 1 && first < last)
+                first++;
+            if (first < last) {
+                int temp = arr[arr[first] - 1];
+                arr[arr[first] - 1] = arr[first];
+                arr[first] = temp;
+                swaps++;
+            }
+        }
+        return swaps;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        System.out.println(res);
+
+        scanner.close();
+    }
+}
