1cb1011aa48bac8355b8fd8e2e64c4973a6e1a38
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-24 09:33:50 -0500

    AVeryBigSum

---
+++ src/AVeryBigSum/Solution.java
@@ -0,0 +1,45 @@
+package AVeryBigSum;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar) {
+        long x=0;
+        for(int i=0;i<ar.length;i++){
+            x+=ar[i];
+        }
+        return x;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] ar = new long[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        long result = aVeryBigSum(ar);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
